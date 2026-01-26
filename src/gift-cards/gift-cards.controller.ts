76b1945cb41dac2a829521fd8162befd2fd1c942
Ricardo <ricardo.dao.ort@gmail.com>
2026-01-26 20:50:13 -0300

    added endpoints from gift-card controller

--- src/gift-cards/gift-cards.controller.ts
+++ src/gift-cards/gift-cards.controller.ts
@@ -1,6 +1,10 @@
-import {Controller,Post,Body,Get,HttpCode,HttpStatus} from '@nestjs/common';
+import {Controller,Post,Body,Get,HttpCode,HttpStatus, ParseUUIDPipe, Param, Put} from '@nestjs/common';
 import { GiftCardsService } from './gift-cards.service';
 import { GiftCardCatalogDto } from './dto/gift-card-catalog.dto';
+import { CreateGiftCardDto } from './dto/create-gift-card.dto';
+import { GiftCard } from './entities/gift-cards.entity';
+import { UpdateGiftCardDto } from './dto/update-gift-card.dto';
+
 
 @Controller('gift-cards')
 export class GiftCardsController {
@@ -11,6 +15,37 @@ export class GiftCardsController {
     async GiftCardCatalog(): Promise<GiftCardCatalogDto[]> {
         return this.giftCardsService.getGiftCardCatalog();
     }
+
+    @Post('create')
+    @HttpCode(HttpStatus.CREATED)
+    async createCard(@Body()data: CreateGiftCardDto):Promise<GiftCard>{
+        return this.giftCardsService.createGiftCard(data);
+    }
+
+    @Get('all')
+    @HttpCode(HttpStatus.OK)
+    async findAllCards():Promise<GiftCard[]>{
+        return this.giftCardsService.findAll();
+    }
+
+    @Get('shop/:id')
+    @HttpCode(HttpStatus.OK)
+    async findByStoreId(@Param('id', ParseUUIDPipe) id: string):Promise<GiftCard[]>{
+        return this.giftCardsService.findByShop(id);
+    }
+    
+    @Get('detail/:id')
+    @HttpCode(HttpStatus.OK)
+    async findById(@Param('id', ParseUUIDPipe) id: string): Promise<GiftCard> {
+        return this.giftCardsService.findById(id);
+    }
+
+    @Put('update/:id')
+    @HttpCode(HttpStatus.OK)
+    async updateCard(@Param('id', ParseUUIDPipe) id: string, @Body() data: UpdateGiftCardDto): Promise<GiftCard> {
+        return this.giftCardsService.updateCard(id, data);
+    }
+
 
 
 }
