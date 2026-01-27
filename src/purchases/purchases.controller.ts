3c6052ac1727a2f1209e95e4fcd5f07204e0a4ff
Ricardo <ricardo.dao.ort@gmail.com>
2026-01-27 01:26:12 -0300

    feature: added endpoints for purchase controller payment, user/:userId ,id

--- src/purchases/purchases.controller.ts
+++ src/purchases/purchases.controller.ts
@@ -1,7 +1,27 @@
-import {Controller,Post,Body,Get,HttpCode,HttpStatus} from'@nestjs/common';
+import {Controller,Post,Body,Get,HttpCode,HttpStatus, Param} from'@nestjs/common';
 import { PurchasesService } from './purchases.service';
+import { CreatePurchaseDto } from './dto/create-purchase.dto';
 
 @Controller('purchases')
 export class PurchasesController {
     constructor (private readonly purchasesService: PurchasesService) {}
+
+
+
+    @Post('payment')
+    @HttpCode (HttpStatus.CREATED)
+    async createPurchase(@Body() createPurchaseDto:CreatePurchaseDto) {
+        return this.purchasesService.purchase(createPurchaseDto);
+    }
+
+    @Get('user/:userId')
+    async getPurchasesByUser(@Param('userId') userId: string) {
+        return this.purchasesService.findByUser(userId);
+    }
+
+    @Get(':id')
+    async getPurchaseById(@Param('id') id: string) {
+        return this.purchasesService.findbyId(id);
+    }
+
 }
