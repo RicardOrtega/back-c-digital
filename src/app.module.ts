2c282598dc7968f139b2c4fc7f7292e3f1747d72
Ricardo <ricardo.dao.ort@gmail.com>
2026-01-27 14:05:28 -0300

    refactor: user.service changed error to http exceptions features: added cors, rate limiting and security headers

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,12 +10,20 @@ import { GiftCardsModule } from './gift-cards/gift-cards.module';
 import { PurchasesModule } from './purchases/purchases.module';
 import { RedemptionHistoryModule } from './redemption-history/redemption-history.module';
 import { GiftCardCodesModule } from './gift-cards-codes/gift-card-codes.module';
-
+import { ThrottlerModule } from '@nestjs/throttler/dist/throttler.module';
+import { APP_GUARD } from '@nestjs/core';
+import { ThrottlerGuard } from '@nestjs/throttler/dist/throttler.guard';
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    ThrottlerModule.forRoot([
+      {
+        ttl: 60000,
+        limit: 10,
+      }
+    ]),
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
@@ -43,6 +51,6 @@ import { GiftCardCodesModule } from './gift-cards-codes/gift-card-codes.module';
     RedemptionHistoryModule
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [{provide: APP_GUARD, useClass: ThrottlerGuard}, AppService],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common/pipes/validation.pipe';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule,{
     logger: ['error', 'warn', 'log', 'debug' ,'verbose'],
   });
+
+  app.enableCors({
+    origin: '*',
+    credentials: true,
+  });
+
+  app.use(helmet());
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
