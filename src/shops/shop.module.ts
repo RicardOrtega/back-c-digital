2a0475cc37b02f8b3997af531871c069b0d35b35
Ricardo <ricardo.dao.ort@gmail.com>
2026-01-27 13:17:48 -0300

    features: added import AuthModule and applied JwtAuthGuard to shop controller

--- src/shops/shop.module.ts
+++ src/shops/shop.module.ts
@@ -3,10 +3,12 @@ import { TypeOrmModule } from "@nestjs/typeorm";
 import { Shop } from "./entities/shop.entity";
 import { ShopService } from "./shop.service";
 import { ShopController } from "./shop.controller";
+import { AuthModule } from "src/auth/auth.module";
 
 @Module({
     imports: [
         TypeOrmModule.forFeature([Shop]),
+        AuthModule
     ],
     controllers: [ShopController],
     providers: [ShopService],
