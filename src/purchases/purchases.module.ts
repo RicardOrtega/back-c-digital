007c095ee552e0c871a1b13fc7ad622024e912eb
Ricardo <ricardo.dao.ort@gmail.com>
2026-01-27 11:41:22 -0300

    feature: added import controllers and service to redemption-history fix: added missing imports to purchases module

--- src/purchases/purchases.module.ts
+++ src/purchases/purchases.module.ts
@@ -3,10 +3,14 @@ import { TypeOrmModule } from "@nestjs/typeorm";
 import { Purchase } from "./entities/purchase.entity";
 import { PurchasesController } from "./purchases.controller";
 import { PurchasesService } from "./purchases.service";
+import { GiftCardCodesModule } from "src/gift-cards-codes/gift-card-codes.module";
+import { GiftCardsModule } from "src/gift-cards/gift-cards.module";
 
 @Module({
     imports: [
         TypeOrmModule.forFeature([Purchase]),
+        GiftCardsModule,
+        GiftCardCodesModule
     ],
     controllers: [PurchasesController],
     providers: [PurchasesService],
--- src/redemption-history/redemption-history.module.ts
+++ src/redemption-history/redemption-history.module.ts
@@ -1,13 +1,15 @@
 import { Module } from "@nestjs/common";   
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { RedemptionHistory } from "./entities/redemption-history.entity";
+import { RedemptionHistoryController } from "./redemption-history.controller";
+import { RedemptionHistoryService } from "./redemption-history.service";
 
 @Module({
     imports: [
         TypeOrmModule.forFeature([RedemptionHistory]),
     ],
-    controllers: [],
-    providers: [],
-    exports: [],
+    controllers: [RedemptionHistoryController],
+    providers: [RedemptionHistoryService],
+    exports: [RedemptionHistoryService],
 })
 export class RedemptionHistoryModule {}
