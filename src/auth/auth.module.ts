438bcbe621d5ae299c92080cdbfbea20b901ec9d
Ricardo <ricardo.dao.ort@gmail.com>
2026-01-27 12:13:14 -0300

    feature: add authentication module and guard

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,22 @@
+import { Module } from "@nestjs/common";
+import { JwtModule } from "@nestjs/jwt";
+import { ConfigModule,ConfigService } from "@nestjs/config";
+import {JwtStrategy} from "./strategies/jwt.strategy";
+
+@Module({
+    imports: [
+        JwtModule.registerAsync({
+            imports: [ConfigModule],
+            inject: [ConfigService],
+            useFactory: async (configService: ConfigService) => ({
+                secret: configService.get('JWT_SECRET') || 'defaultSecret' ,
+                signOptions: { 
+                    expiresIn: (configService.get('JWT_EXPIRES_IN') || '24h')
+                 },
+            }),
+        }),
+    ],
+    providers: [JwtStrategy],
+    exports: [JwtModule],
+})
+export class AuthModule {}
