7b256f307c07bc54f410441d8ba34a5d46c57efd
Ricardo <ricardo.dao.ort@gmail.com>
2026-01-28 03:41:38 -0300

    fix: code generation from 8 chars to 16

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common/pipes/validation.pipe';
 import helmet from 'helmet';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule,{
@@ -9,11 +10,12 @@ async function bootstrap() {
   });
 
   app.enableCors({
-    origin: '*',
+    origin: 'http://localhost:3000',
     credentials: true,
   });
 
   app.use(helmet());
+  app.use(cookieParser());
 
   app.useGlobalPipes(
     new ValidationPipe({
